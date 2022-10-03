dccc5cea4e9c6021249a0b4408ff79844c089182
ARTUR BERNARDO SILVA REIS <67850791+arturberna@users.noreply.github.com>
2022-10-03 07:59:39 -0300

    Add files via upload

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    public float velocidade = 0.075f;
+    public Renderer fundo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 deslocamento = new Vector2(velocidade*Time.deltaTime,0);
+        fundo.material.mainTextureOffset+=deslocamento;
+    }
+}
---
+++ Assets/Scripts/MoveAviao.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAviao : MonoBehaviour
+{
+    public GameObject aviao;
+    public float velocidade = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // //código para mudar de direção
+       
+        // if(gameObject.transform.position.y>3){
+        //   velocidade*= -1;
+        // }else if(gameObject.transform.position.y< -3)
+        // {
+        //     velocidade*= -1;
+        // }
+        // transform.Translate(new Vector2(0,velocidade*Time.deltaTime));
+
+        if(Input.GetKey(KeyCode.UpArrow)){
+            transform.Translate(new Vector3(0,0.2f,0));
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            transform.Translate(new Vector3(0,-0.2f,0));
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.Translate(new Vector3(-0.2f,0,0));
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.Translate(new Vector3(0.2f,0,0));
+        }
+
+    }
+}
