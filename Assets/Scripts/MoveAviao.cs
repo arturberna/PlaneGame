7f5beec55b5f698d8008eb5f859e3efa3a9c0f1e
ARTUR BERNARDO SILVA REIS <67850791+arturberna@users.noreply.github.com>
2022-10-20 08:19:39 -0300

    Add files via upload

---
+++ Assets/Scripts/MoveAviao.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAviao : MonoBehaviour
+{
+    public GameObject aviao;
+    public float velocidade = 1.15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Método que é chamado quando a tela é atualizada
+    void Update()
+    {
+        
+        if(Input.GetKey(KeyCode.UpArrow)){
+            transform.Translate(new Vector3(0,0.2f,0));
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            transform.Translate(new Vector3(0,-0.2f,0));
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.Translate(new Vector3(-0.2f,0,0));
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.Translate(new Vector3(0.2f,0,0));
+        }
+
+    }
+}
